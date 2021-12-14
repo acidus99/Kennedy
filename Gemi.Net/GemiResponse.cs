3244eb938bb5307543894632772035bbdda91902
Billy Hoffman <billymhoffman@gmail.com>
2021-12-13 20:00:33 -0500

    ConnectStatus as explicit int

--- Gemi.Net/GemiResponse.cs
+++ Gemi.Net/GemiResponse.cs
@@ -154,11 +154,11 @@ namespace Gemi.Net
     /// Status of the network connection made for a Gemini request.
     /// Used to show errors at the network level vs protocol level
     /// </summary>
-    public enum ConnectStatus
+    public enum ConnectStatus : int
     {
-        Unknown,
-        Success,
-        Error
+        Unknown = 0,
+        Success = 1,
+        Error = 2,
     }
 
 
