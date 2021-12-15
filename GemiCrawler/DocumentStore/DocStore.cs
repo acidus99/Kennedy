9c969fb180bad0a7df3cf7b0e1fffcfbf4373525
Billy Hoffman <billymhoffman@gmail.com>
2021-12-14 20:03:49 -0500

    Replace File-system backed DocStore with one backed by an ObjectStore

--- GemiCrawler/DocumentStore/DocStore.cs
+++ GemiCrawler/DocumentStore/DocStore.cs
@@ -1,101 +1,33 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Gemi.Net;
-using GemiCrawler.Utils;
-
-using GemiCrawler.Modules;
+using System.Security.Cryptography;
 
 namespace GemiCrawler.DocumentStore
 {
+    /// <summary>
+    /// Document store implemented using an Object Store, backed onto a disk
+    /// </summary>
     public class DocStore : IDocumentStore
     {
+        ObjectStore store;
 
-        string pageStorageDir;
-
-        public DocStore(string path)            
+        public DocStore(string outputDir)
         {
-
-            pageStorageDir = path;
-            if (Directory.Exists(pageStorageDir))
-            {
-                DirectoryInfo di = new DirectoryInfo(pageStorageDir);
-
-                foreach (FileInfo file in di.EnumerateFiles())
-                {
-                    file.Delete();
-                }
-                foreach (DirectoryInfo dir in di.EnumerateDirectories())
-                {
-                    dir.Delete(true);
-                }
-
-                Directory.Delete(pageStorageDir);
-            }
-        }
-
-
-        private string GetStorageFilename(GemiUrl url)
-        {
-            var filename = Path.GetFileName(url.Path);
-            return (filename.Length > 0) ? filename : "index.gmi";
-        }
-
-        private string GetSavePath(GemiUrl url)
-        {
-            var dir = GetStorageDirectory(url);
-            var file = GetStorageFilename(url);
-            return dir + file;
-        }
-
-        private string GetStorageDirectory(GemiUrl url)
-        {
-            string hostDir = (url.Port == 1965) ? url.Hostname : $"{url.Hostname} ({url.Port})";
-
-            string path = Path.GetDirectoryName(url.Path);
-            if(string.IsNullOrEmpty(path))
-            {
-                path = "/";
-            }
-            if(!path.EndsWith('/'))
-            {
-                path += "/";
-            }
-
-            return $"{pageStorageDir}{hostDir}{path}";
+            store = new ObjectStore(outputDir);
         }
 
         public string StoreDocument(GemiUrl url, GemiResponse resp)
         {
-            var path = "";
-
+            var key = "";
             if (resp.IsSuccess & resp.HasBody)
             {
-                var dir = GetStorageDirectory(url);
-                var file = GetStorageFilename(url);
-                path = dir + file;
-
-                try
+                key = Convert.ToHexString(MD5.HashData(resp.BodyBytes)).ToLower();
+                if(!store.StoreObject(key, resp.BodyBytes))
                 {
-                    Directory.CreateDirectory(dir);
-                }
-                catch (Exception)
-                { }
-
-                try
-                {
-                    //if for some reason the file already exists, don't do anything
-                    if (!File.Exists(path))
-                    {
-                        File.WriteAllBytes(path, resp.BodyBytes);
-                    }
-                }
-                catch (Exception)
-                {
+                    throw new ApplicationException("Failed to store resp!");
                 }
             }
-            return path;
+            return key;
         }
     }
 }
---
+++ GemiCrawler/DocumentStore/ObjectStore.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Gemi.Net;
+using GemiCrawler.Utils;
+using System.IO.Compression;
+
+using GemiCrawler.Modules;
+
+namespace GemiCrawler.DocumentStore
+{
+    /// <summary>
+    /// disk-backed KV store, using a directory structure
+    /// </summary>
+    public class ObjectStore
+    {
+        string rootDir;
+
+        public ObjectStore(string path)
+        {
+            rootDir = path;
+        }
+
+        private string getPrefixDirectoryForKey(string key)
+        {
+            if(key.Length < 4)
+            {
+                return rootDir;
+            }
+            return rootDir + Path.DirectorySeparatorChar + key[0] + key[1] + Path.DirectorySeparatorChar + key[2] + key[3] + Path.DirectorySeparatorChar;
+        }
+
+        public bool StoreObject(string key, byte [] bytes)
+        {
+            var dir = getPrefixDirectoryForKey(key);
+            try
+            {
+                Directory.CreateDirectory(dir);
+                if (!File.Exists(dir + key))
+                {
+                    File.WriteAllBytes(dir + key, bytes);
+                    return true;
+                }
+            } catch (Exception)
+            {
+
+            }
+            return false;
+        }
+
+        public byte [] GetObject(string key)
+        {
+            var dir = getPrefixDirectoryForKey(key);
+            return File.ReadAllBytes(dir + key);
+        }
+    }
+}
