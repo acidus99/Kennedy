accd15f098fb212ace7034844b6a388e2fae9a1a
Billy Hoffman <billymhoffman@gmail.com>
2021-12-17 13:13:11 -0500

    new seeds

--- GemiCrawler/Program.cs
+++ GemiCrawler/Program.cs
@@ -7,19 +7,14 @@ namespace GemiCrawler
     {
         static void Main(string[] args)
         {
-            var crawler = new Crawler(120,500000);
-
-            crawler.AddSeed("gemini://tlgs.one/known-hosts");
+            var crawler = new Crawler(80,500000);
+            
             crawler.AddSeed("gemini://gemini.bortzmeyer.org/software/lupa/lupa-capsules.gmi");
+            crawler.AddSeed("gemini://tlgs.one/known-hosts");
             crawler.AddSeed("gemini://geminispace.info/known-hosts");
-
             crawler.DoCrawl();
 
             return;
         }
-
     }
-
-    
-
 }
