0d3d11ea168fefcf5a76121f767ec43668b09727
Billy Hoffman <billymhoffman@gmail.com>
2021-12-29 15:03:20 -0500

    Create DB context for Link Index

---
+++ GemiCrawler/DocumentIndex/Db/LinkIndexDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+
+namespace GemiCrawler.DocumentIndex.Db
+{
+    public class LinkIndexDbContext : AbstractSqlLiteDbContext
+    {
+        public LinkIndexDbContext(string storageDir)
+            : base(storageDir)
+        { }
+
+        public DbSet<StoredLinkEntry> LinkEntries { get; set; }
+
+        protected override string DbFilename => "link-index.db";
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StoredLinkEntry>()
+                .HasKey(l => new { l.DBSourceDocID, l.DBTargetDocID});
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
