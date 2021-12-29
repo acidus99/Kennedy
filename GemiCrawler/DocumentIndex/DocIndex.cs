b796b1a3720a85814816706e0eb1efe0ea1c63f4
Billy Hoffman <billymhoffman@gmail.com>
2021-12-29 15:03:59 -0500

    use updated model and DBContext

--- GemiCrawler/DocumentIndex/DocIndex.cs
+++ GemiCrawler/DocumentIndex/DocIndex.cs
@@ -1,4 +1,5 @@
-﻿using Gemi.Net;
+﻿using System.Data;
+using Gemi.Net;
 using GemiCrawler.DocumentIndex.Db;
 using System.Collections.Generic;
 
@@ -7,26 +8,32 @@ namespace GemiCrawler.DocumentIndex
     public class DocIndex : IMetaStore
     {
         string StoragePath;
-        DocIndexDbContext db;
-        object locker;
-
 
         public DocIndex(string storagePath)
         {
             StoragePath = storagePath;
-            db = new DocIndexDbContext(storagePath);
-            locker = new object();
+            //create and distory a DBContext to force the DB to be there
+            var db = new DocIndexDbContext(storagePath);
         }
 
         public void Close()
         {
-            //nop
+            ///nop since we are not caching any writes
         }
+
+        private long toLong(ulong ulongValue)
+            => unchecked((long)ulongValue);
+
+        private ulong toULong(long longValue)
+            => unchecked((ulong)longValue);
 
         public void StoreMetaData(GemiUrl url, GemiResponse resp, List<GemiUrl> foundLinks, string storageKey)
         {
-            var storedResp = new StoredDocEntry
+            var entry = new StoredDocEntry
             {
+                DBDocID = toLong(url.DocID),
+                FirstSeen = System.DateTime.Now,
+
                 Url = resp.RequestUrl.NormalizedUrl,
                 Domain = resp.RequestUrl.Hostname,
                 Port = resp.RequestUrl.Port,
@@ -40,9 +47,9 @@ namespace GemiCrawler.DocumentIndex
                 MimeType = resp.MimeType
             };
 
-            lock (locker)
+            using (var db = new DocIndexDbContext(StoragePath))
             {
-                db.Responses.Add(storedResp);
+                db.DocEntries.Add(entry);
                 db.SaveChanges();
             }
         }
