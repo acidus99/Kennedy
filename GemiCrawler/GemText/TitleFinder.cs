f3b45f237aee3256303696ea6c0d1e3b20615b61
Billy Hoffman <billymhoffman@gmail.com>
2021-12-30 00:48:27 -0500

    basic gemtext title extractor

---
+++ GemiCrawler/GemText/TitleFinder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+using Gemi.Net;
+
+namespace GemiCrawler.GemText
+{
+    /// <summary>
+    /// Attempts to determine a "title" for a gemtext page
+    /// </summary>
+    public static class TitleFinder
+    {
+        public static string ExtractTitle(GemiResponse resp)
+        {
+            if (resp.IsSuccess && resp.HasBody && resp.MimeType.StartsWith("text/gemini"))
+            {
+                var t = resp.BodyText.Split("\n")
+                    .Where(x => x.TrimStart().StartsWith("# ") && x.TrimStart().Length > 2)
+                    .FirstOrDefault().Substring(2);
+                return t;
+            }
+
+            return "";
+        }
+    }
+}
