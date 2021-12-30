139720bdf7e297d3c17f150cb8690e95dbe43964
Billy Hoffman <billymhoffman@gmail.com>
2021-12-29 22:26:11 -0500

    Adopting unified DocIndex class

--- GemiCrawler/DocumentIndex/DocIndex.cs
+++ GemiCrawler/DocumentIndex/DocIndex.cs
@@ -2,10 +2,11 @@
 using Gemi.Net;
 using GemiCrawler.DocumentIndex.Db;
 using System.Collections.Generic;
+using EFCore.BulkExtensions;
 
 namespace GemiCrawler.DocumentIndex
 {
-    public class DocIndex : IMetaStore
+    public class DocIndex
     {
         string StoragePath;
 
@@ -14,6 +15,7 @@ namespace GemiCrawler.DocumentIndex
             StoragePath = storagePath;
             //create and distory a DBContext to force the DB to be there
             var db = new DocIndexDbContext(storagePath);
+            db.Database.EnsureCreated();
         }
 
         public void Close()
@@ -27,12 +29,15 @@ namespace GemiCrawler.DocumentIndex
         private ulong toULong(long longValue)
             => unchecked((ulong)longValue);
 
-        public void StoreMetaData(GemiUrl url, GemiResponse resp, List<GemiUrl> foundLinks, string storageKey)
+        public void StoreMetaData(GemiUrl url, GemiResponse resp, List<GemiUrl> foundLinks)
         {
             var entry = new StoredDocEntry
             {
                 DBDocID = toLong(url.DocID),
                 FirstSeen = System.DateTime.Now,
+                LastVisit = System.DateTime.Now,
+
+                BodyHash = resp.BodyHash,
 
                 Url = resp.RequestUrl.NormalizedUrl,
                 Domain = resp.RequestUrl.Hostname,
@@ -53,5 +58,25 @@ namespace GemiCrawler.DocumentIndex
                 db.SaveChanges();
             }
         }
+
+        public void StoreLinks(GemiUrl sourcePage, List<GemiUrl> links)
+        {
+            using (var db = new DocIndexDbContext(StoragePath))
+            {
+                db.BulkInsert(links.Distinct().Select(target => new StoredLinkEntry
+                {
+                    SourceURL = sourcePage.NormalizedUrl,
+                    TargetURL = target.NormalizedUrl,
+                    DBSourceDocID = toLong(sourcePage.DocID),
+                    DBTargetDocID = toLong(target.DocID),
+                    LinkText = "Some magic text!"
+                }).ToList());
+
+                db.SaveChanges();
+                //7102968342834367033
+            }
+        }
+
+
     }
 }
