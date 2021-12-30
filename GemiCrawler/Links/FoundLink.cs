2c57c6653fda9de7017b9dce74b398cca28ab0a9
Billy Hoffman <billymhoffman@gmail.com>
2021-12-29 23:26:56 -0500

    Refactor LinkFinder + explicit FoundLink object w/ meta data

---
+++ GemiCrawler/Links/FoundLink.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Gemi.Net;
+namespace GemiCrawler.Links
+{
+    public class FoundLink : IEquatable<FoundLink>
+    {
+        public GemiUrl Url { get; set; }
+        public bool IsExternal { get; set; }
+        public string LinkText { get; set; }
+
+        /// <summary>
+        /// What makes a FoundLink unique is really just its URL. We are properly ignoring links to the same thing with different LinkText
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(FoundLink other)
+            => other != null && Url.Equals(other.Url);
+
+        public override bool Equals(object obj)
+            => Equals(obj as GemiUrl);
+
+        public override int GetHashCode()
+            => Url.GetHashCode();
+
+    }
+}
---
+++ GemiCrawler/Links/LinkFinder.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using Gemi.Net;
+using System.Linq;
+
+namespace GemiCrawler.Links
+{
+    /// <summary>
+    /// Finds hyperlinks in Gemini Text Documents
+    /// </summary>
+    public static class LinkFinder
+    {
+        static readonly Regex linkLine = new Regex(@"^=>\s+([^\s]+)\s*(.*)", RegexOptions.Compiled);
+
+        public static List<FoundLink> ExtractLinks(GemiUrl request, GemiResponse resp)
+        {
+            var links = new List<FoundLink>();
+
+            if(resp.IsRedirect)
+            {
+                var foundLink = Create(request, resp.Meta);
+                if(foundLink != null)
+                {
+                    links.Add(foundLink);
+                }
+            }
+            else if(resp.IsSuccess && resp.HasBody && resp.MimeType.StartsWith("text/gemini"))
+            {
+                var foundLinks =
+                            (from line in resp.BodyText.Split("\n")
+                             let match = linkLine.Match(line)
+                             where match.Success
+                             let link = Create(request, match)
+                             where link != null
+                             select link);
+                links.AddRange(foundLinks);
+            }
+
+            return links;
+        }
+
+        private static FoundLink Create(GemiUrl pageUrl, Match match)
+            => Create(pageUrl, match.Groups[1].Value, getLinkText(match));
+
+        private static FoundLink Create(GemiUrl pageUrl, string foundUrl, string linkText = "")
+        {
+            var newUrl = GemiUrl.MakeUrl(pageUrl, foundUrl);
+            //ignore anything that doesn't resolve properly, or isn't to a gemini:// URL
+            if (newUrl == null)
+            {
+                return null;
+            }
+            return new FoundLink
+            {
+                Url = newUrl,
+                IsExternal = (newUrl.Authority != pageUrl.Authority),
+                LinkText = linkText
+            };
+        }
+
+        /// <summary>
+        /// gives us the text, if any, used with this link
+        /// </summary>
+        /// <param name="match"></param>
+        /// <returns></returns>
+        private static string getLinkText(Match match)
+            => (match.Groups.Count > 2) ? match.Groups[2].Value : "";
+
+    }
+
+
+}
