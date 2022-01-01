813244dc2e4ae5f20476aefbf526803bd4fd309f
Billy Hoffman <billymhoffman@gmail.com>
2022-01-01 12:30:38 -0500

    testing restarting a crawl

--- GemiCrawler/Program.cs
+++ GemiCrawler/Program.cs
@@ -7,11 +7,19 @@ namespace GemiCrawler
     {
         static void Main(string[] args)
         {
-            var crawler = new Crawler(50,500000);
-            
-            crawler.AddSeed("gemini://gemini.bortzmeyer.org/software/lupa/lupa-capsules.gmi");
-            crawler.AddSeed("gemini://tlgs.one/known-hosts");
-            crawler.AddSeed("gemini://geminispace.info/known-hosts");
+            var crawler = new Crawler(80,300000);
+
+            bool doNew = true;
+            if (doNew)
+            {
+                crawler.AddSeed("gemini://gemini.bortzmeyer.org/software/lupa/lupa-capsules.gmi");
+                crawler.AddSeed("gemini://tlgs.one/known-hosts");
+                crawler.AddSeed("gemini://geminispace.info/known-hosts");
+            }
+            else
+            {
+                crawler.LoadPreviousResults();
+            }
             crawler.DoCrawl();
 
             return;
