118fb757ce23c529333e7079865cc3d27188fde9
Billy Hoffman <acidus99@users.noreply.github.com>
2022-03-01 08:51:38 -0500

    Extract titles from alt text

--- Crawler/GemText/TitleFinder.cs
+++ Crawler/GemText/TitleFinder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 using Gemini.Net;
 
@@ -7,20 +8,55 @@ namespace Kennedy.Crawler.GemText
 {
     /// <summary>
     /// Attempts to determine a "title" for a gemtext page
+    /// Rules:
+    /// - Look for any header
+    /// - Look the first preformatted text section for an alt text (used for
+    ///     ascii art logos)
     /// </summary>
     public static class TitleFinder
     {
+
+        static readonly Regex headingRegex = new Regex(@"^(#+)\s*(.+)", RegexOptions.Compiled);
+
         public static string ExtractTitle(GeminiResponse resp)
         {
             if (resp.IsSuccess && resp.HasBody && resp.MimeType.StartsWith("text/gemini"))
             {
-                var t = resp.BodyText.Split("\n")
-                    .Where(x => x.StartsWith("# ") && x.Length > 2)
-                    .FirstOrDefault();
-                return t == null ? "" : t.Substring(2);
+                return ExtractTile(resp.BodyText);
             }
-
             return "";
+        }
+
+        public static string ExtractTile(string gemText)
+        {
+            var title = TryHeaders(gemText);
+            if(title.Length > 0)
+            {
+                return title;
+            }
+            return TryPreformatted(gemText);
+        }
+
+        private static string TryHeaders(string gemText)
+        {
+            var t = gemText.Split("\n")
+                   .Where(x => x.StartsWith("```") && x.Length > 2)
+                   .FirstOrDefault() ?? "";
+            var match = headingRegex.Match(t);
+            if (match.Success)
+            {
+                t = match.Groups[2].Value.Trim();
+            }
+            return t ?? "";
+        }
+
+        private static string TryPreformatted(string gemText)
+        {
+            var t = gemText.Split("\n")
+                   .Where(x => x.StartsWith("```"))
+                   .FirstOrDefault() ?? "";
+
+            return (t.Length > 3) ? t.Substring(3).Trim() : "";
         }
     }
 }
