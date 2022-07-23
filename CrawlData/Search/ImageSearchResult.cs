e265b616894b8bb051fb396d036a2d0ba1b5a3bc
Acidus <acidus99@users.noreply.github.com>
2022-07-23 13:47:41 -0400

    add Image Search Engine

---
+++ CrawlData/Search/ImageSearchResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Gemini.Net;
+
+namespace Kennedy.CrawlData.Search
+{
+    public class ImageSearchResult
+    {
+        public GeminiUrl Url { get; set; }
+        public int BodySize { get; set; }
+        public string Snippet { get; set; }
+        public long DBDocID { get; set; }
+
+        public string ImageType { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public string Favicon { get; set; }
+        public bool BodySaved { get; set; }
+    }
+}
