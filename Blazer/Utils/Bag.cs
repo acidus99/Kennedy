8c9a59428ae002345f085270c8d127ec29b97123
Acidus <acidus99@users.noreply.github.com>
2022-11-25 15:30:10 -0500

    Create Bag.cs

---
+++ Blazer/Utils/Bag.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Kennedy.Crawler.Utils
+{
+    /// <summary>
+    /// Generic Object Bag - Holds objects and keeps a count of how many times the same object has been added
+    /// to the bag. Very handy to throw in a bunch of items and get out a list of unique objects
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Bag<T>
+    {
+        object locker;
+
+        public int TotalAdds { get; private set; }
+
+        Dictionary<T, int> bag;
+
+        public Bag()
+        {
+            bag = new Dictionary<T, int>();
+            locker = new object();
+        }
+
+        public void Clear()
+            => bag.Clear();
+
+        public int Add(T t)
+        {
+            int count = 0;
+
+            lock (locker)
+            {
+                TotalAdds += 1;
+                if (!bag.ContainsKey(t))
+                {
+                    count = 1;
+                    bag[t] = 1;
+                }
+                else
+                {
+                    count = bag[t] + 1;
+                    bag[t] = count;
+                }
+            }
+            return count;
+        }
+
+        public void AddRange(IEnumerable<T> enumberable)
+        {
+            foreach(T t in enumberable)
+            {
+                Add(t);
+            }
+        }
+
+        public bool Contains(T t)
+        {
+            lock(locker)
+            {
+                return bag.ContainsKey(t);
+            }
+        }
+
+        public IEnumerable<T> GetValues()
+            => bag.Keys;
+
+        public List<Tuple<T, int>> GetSortedValues(int atLeast = 1)
+        {
+            return bag.Keys
+                .Select(x => new Tuple<T, int>(x, bag[x]))
+                .Where(x => (x.Item2 >= atLeast)).OrderByDescending(x => x.Item2).ToList();
+        }
+
+        public int UniqueItems
+            =>bag.Keys.Count;
+    }
+}
