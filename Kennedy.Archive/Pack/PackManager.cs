d982caa07ee6b08042955aa6b6791dee1c69cf08
Acidus <acidus99@users.noreply.github.com>
2023-04-01 17:03:36 -0400

    check keys

--- Kennedy.Archive/Pack/PackManager.cs
+++ Kennedy.Archive/Pack/PackManager.cs
@@ -6,6 +6,8 @@
     public class PackManager
     {
         string ArchiveRoot;
+        char[] InvalidPathChars;
+        char[] InvalidFileChars;
 
         public PackManager(string path)
         {
@@ -14,6 +16,36 @@
             {
                 ArchiveRoot += Path.DirectorySeparatorChar;
             }
+            InvalidPathChars = Path.GetInvalidPathChars();
+            InvalidFileChars = Path.GetInvalidFileNameChars();
+        }
+
+        /// <summary>
+        /// checks that a key doesn't contain illegal characters (e.g. illegal characters for path or filename)
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        private bool IsKeyIsValid(string key)
+        {
+            for (int i = 0, len = key.Length; i < len; i++)
+            {
+                char c = key[i];
+                if (i < 4 && InvalidPathChars.Contains(c))
+                {
+                    return false;
+                }
+                if (InvalidFileChars.Contains(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private string getPathForPackName(string packName)
+        {
+            var path = ArchiveRoot + Path.DirectorySeparatorChar + packName[0] + packName[1] + Path.DirectorySeparatorChar +
+                packName[2] + packName[3] + Path.DirectorySeparatorChar;
         }
 
         public PackFile GetPack(string packName)
@@ -23,6 +55,11 @@
                 throw new ArgumentException($"PackID is too short! Expected > 4, got {packName.Length}");
             }
 
+            if (!IsKeyIsValid(packName))
+            {
+                throw new ArgumentException("Packname contains invalid characters", "packName");
+            }
+
             var path = ArchiveRoot + Path.DirectorySeparatorChar + packName[0] + packName[1] + Path.DirectorySeparatorChar +
                 packName[2] + packName[3]+ Path.DirectorySeparatorChar;
 
@@ -30,6 +67,14 @@
             Directory.CreateDirectory(path);
 
             return new PackFile(path + packName);
+        }
+
+        public void DeletePack(string packName)
+        {
+            if (!IsKeyIsValid(packName))
+            {
+                throw new ArgumentException("Packname contains invalid characters", "packName");
+            }
         }
     }
 }
