4e4a50f5c60db7dbb2a7b9f0f08691d5849f8325
Acidus <acidus99@users.noreply.github.com>
2023-04-12 15:40:57 -0400

    archive non-body responses

--- Archive/Archiver.cs
+++ Archive/Archiver.cs
@@ -27,8 +27,68 @@ namespace Kennedy.Archive
             packManager = new PackManager(packsPath);
         }
 
-		public bool ArchiveContent(DateTime captured, GeminiUrl url, int statusCode, string meta, byte[] contentData, bool isPublic = true)
-		{
+        /// <summary>
+        /// Archives a response without a body
+        /// </summary>
+        /// <param name="captured"></param>
+        /// <param name="url"></param>
+        /// <param name="statusCode"></param>
+        /// <param name="meta"></param>
+        /// <param name="isPublic"></param>
+        /// <returns></returns>
+        public bool ArchiveResponse(DateTime captured, GeminiUrl url, int statusCode, string meta, bool isPublic = true)
+        {
+            //are the capture times the same? If so, don't save it, because its a dupe
+            if (Context.Snapshots
+                .Where(x => x.UrlId == url.ID && x.Captured == captured)
+                .Any())
+            {
+                return false;
+            }
+
+            var urlEntry = Context.Urls.Where(x => x.Id == url.ID).FirstOrDefault();
+            if (urlEntry == null)
+            {
+                urlEntry = new Url(url)
+                {
+                    IsPublic = isPublic
+                };
+                Context.Urls.Add(urlEntry);
+                //need to save for foreign key constraint
+                Context.SaveChanges();
+            }
+
+            var snapshot = new Snapshot
+            {
+                Captured = captured,
+                StatusCode = statusCode,
+                Offset = null,
+                Size = null,
+                ContentType = null,
+                Meta = meta,
+                DataHash = null,
+                Url = urlEntry,
+                UrlId = urlEntry.Id,
+            };
+
+            urlEntry.Snapshots.Add(snapshot);
+            Context.Urls.Update(urlEntry);
+            Context.Snapshots.Add(snapshot);
+            Context.SaveChanges();
+
+            return true;
+        }
+
+        public bool ArchiveResponse(DateTime captured, GeminiUrl url, int statusCode, string meta, byte[] contentData, bool isPublic = true)
+        {
+            //are the capture times the same? If so, don't save it, because its a dupe
+            if (Context.Snapshots
+                .Where(x => x.UrlId == url.ID && x.Captured == captured)
+                .Any())
+            {
+                return false;
+            }
+
             var urlEntry = Context.Urls.Where(x => x.Id == url.ID).FirstOrDefault();
             bool newUrl = false;
             if (urlEntry == null)
@@ -57,9 +117,6 @@ namespace Kennedy.Archive
                 .Where(x => x.UrlId == urlEntry.Id &&
                         x.DataHash == dataHash).FirstOrDefault();
 
-            //assume that we will add this to the archive
-            bool shouldAddSnapshot = true;
-
             var snapshot = new Snapshot
             {
                 Captured = captured,
@@ -81,23 +138,14 @@ namespace Kennedy.Archive
             {
                 //is same as existing
                 snapshot.Offset = previousSnapshot.Offset;
-
-                //are the capture times the same? If so, don't save it, because its a dupe
-                if (snapshot.Captured == previousSnapshot.Captured)
-                {
-                    shouldAddSnapshot = false;
-                }
             }
 
-            if (shouldAddSnapshot)
-            {
-                urlEntry.Snapshots.Add(snapshot);
-                Context.Urls.Update(urlEntry);
-                Context.Snapshots.Add(snapshot);
-                Context.SaveChanges();
-            }
+            urlEntry.Snapshots.Add(snapshot);
+            Context.Urls.Update(urlEntry);
+            Context.Snapshots.Add(snapshot);
+            Context.SaveChanges();
 
-            return shouldAddSnapshot;
+            return true;
         }
 
         private long GetDataHash(byte[] body)
