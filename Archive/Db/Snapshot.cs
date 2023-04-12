63eab32f10118a48949e30aa4b7ae5d28ee5da26
Acidus <acidus99@users.noreply.github.com>
2023-04-12 15:40:44 -0400

    offset/size/hash/contenttype can be null to support errors, redirects, etc

--- Archive/Db/Snapshot.cs
+++ Archive/Db/Snapshot.cs
@@ -10,20 +10,20 @@ namespace Kennedy.Archive.Db
 {
 	[Table("Snapshots")]
 	[Index(nameof(DataHash))]
-	public class Snapshot
+    public class Snapshot
 	{
 		[Key]
 		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public long Id { get; set; }
 
-		public long Offset { get; set; }
-		public long Size { get; set; }
+		public long? Offset { get; set; }
+		public long? Size { get; set; }
 
 		public int StatusCode { get; set; }
 
-		public long DataHash { get; set; }
+		public long? DataHash { get; set; }
 
-		public ContentType ContentType { get; set; }
+		public ContentType? ContentType { get; set; }
 
 		public DateTime Captured { get; set; }
 
@@ -33,9 +33,23 @@ namespace Kennedy.Archive.Db
 
         public Url Url { get; set; }
 
-		public bool IsGemtext
-			=> (ContentType == ContentType.Text) && Meta.StartsWith("text/gemini");
+		/// <summary>
+		/// Does this snapshot have body content that is stored in the Pack?
+		/// </summary>
+		public bool HasBodyContent
+			=> Offset.HasValue && Size.HasValue && DataHash.HasValue;
 
+		public bool IsGemtext
+		{
+			get
+            {
+				if(ContentType == null)
+				{
+					return false;
+				}
+				return (this.ContentType.Value == Data.ContentType.Text) && Meta.StartsWith("text/gemini");
+            }
+		}
     }
 	
 }
