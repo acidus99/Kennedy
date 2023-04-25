e31b68ca5b99336a6dbf9432bb6bc202b47a3c6d
Acidus <acidus99@users.noreply.github.com>
2023-04-25 07:49:30 -0400

    create home for indexer process

---
+++ Indexer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Gemini.Net;
+
+using Kennedy.Crawler.Crawling;
+
+using Kennedy.Crawler.TopicIndexes;
+
+
+namespace Kennedy.Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
