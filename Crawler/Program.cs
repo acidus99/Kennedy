c69a436f84358548113d7ae48a9b8f621acb7780
Acidus <acidus99@users.noreply.github.com>
2023-04-25 09:45:59 -0400

    remove hashtag/mentions work from Crawler

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -3,9 +3,6 @@ using Gemini.Net;
 
 using Kennedy.Crawler.Crawling;
 
-using Kennedy.Crawler.TopicIndexes;
-
-
 namespace Kennedy.Crawler
 {
     class Program
@@ -14,27 +11,13 @@ namespace Kennedy.Crawler
         {
             HandleArgs(args);
 
-            //Support.GraphGenerator graph = new Support.GraphGenerator(CrawlerOptions.DataStore);
-
-            //Support.SiteHealthReport report = new Support.SiteHealthReport(CrawlerOptions.DataStore);
-            //report.WriteReport("/tmp/report.gmi", "gemi.dev");
-            //return;
-
-            //var url = "gemini://billy.flounder.online/tech-links.gmi";
-            //var url = "gemini://makeworld.gq/cgi-bin/gemini-irc";
-            //var url = "gemini://gemini.circumlunar.space/docs/faq.gmi";
-            //var url = "gemini://capsule.ghislainmary.fr/photo/";
-            //var url = "gemini://billy.flounder.online/index.gmi";
             var url = "gemini://mozz.us/";
-            //var url = "gemini://marginalia.nu:1965/log";
-            //var url = "gemini://geminispace.info/known-hosts";
 
             var crawler = new WebCrawler(40, 500000);
 
             crawler.AddSeed(url);
             crawler.DoCrawl();
 
-            TopicGenerator.BuildFiles(CrawlerOptions.PublicRoot);
             return;
         }
 
