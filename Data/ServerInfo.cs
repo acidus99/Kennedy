ce02efa8c6759ef9fc15bf4f3131865c6df11775
Acidus <acidus99@users.noreply.github.com>
2023-05-02 17:58:58 -0400

    Domain -> Server

---
+++ Data/ServerInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Kennedy.Data
+{
+	public class ServerInfo
+	{
+        public string Domain { get; set; }
+        public int Port { get; set; }
+        public string Protocol { get; set; }
+
+        public bool IsReachable { get; set; }
+        public string? ErrorMessage { get; set; }
+
+        public long? RobotsUrlID { get; set; }
+        public long? FaviconUrlID { get; set; }
+        public long? SecurityUrlID { get; set; }
+
+        public string? FaviconTxt { get; set; }
+
+        /// <summary>
+        /// When was the record first seen
+        /// </summary>
+        public DateTime FirstSeen { get; set; }
+
+        /// <summary>
+        /// When did we last visit this document
+        /// </summary>
+        public DateTime LastVisit { get; set; }
+
+        /// <summary>
+        /// When did we last successfully visit this document?
+        /// </summary>
+        public DateTime? LastSuccessfulVisit { get; set; }
+    }
+}
--- SearchIndex/Models/Server.cs
+++ SearchIndex/Models/Server.cs
@@ -6,19 +6,38 @@ using Gemini.Net;
 
 namespace Kennedy.SearchIndex.Models
 {
-    [Table("Domains")]
-    public class Domain
+    [Table("Server")]
+    public class Server
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
+        [Column(Order = 0)]
+        public string Domain { get; set; }
 
-        [Column("Domain")]
-        public string DomainName { get; set; }
+        [Key]
+        [Column(Order = 1)]
         public int Port { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        public string Protocol { get; set; }
 
         public bool IsReachable { get; set; }
         public string? ErrorMessage { get; set; }
+
+        /// <summary>
+        /// When was the record first seen
+        /// </summary>
+        public DateTime FirstSeen { get; set; }
+
+        /// <summary>
+        /// When did we last visit this document
+        /// </summary>
+        public DateTime LastVisit { get; set; }
+
+        /// <summary>
+        /// When did we last successfully visit this document?
+        /// </summary>
+        public DateTime? LastSuccessfulVisit { get; set; }
 
         [NotMapped]
         public bool HasSecurityTxt
