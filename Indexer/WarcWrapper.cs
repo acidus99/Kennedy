15b562acd910fef6946a19030dd0f77fecb748bd
Acidus <acidus99@users.noreply.github.com>
2023-05-06 10:07:38 -0400

    mutli-threading wrapper for WarcRecord

---
+++ Indexer/WarcWrapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Warc;
+
+namespace Kennedy.Indexer
+{
+	public class WarcWrapper
+	{
+		object locker = new object();
+
+		WarcParser Parser;
+
+		public int Processed = 0;
+
+		public WarcWrapper(WarcParser warcParser)
+		{
+			Parser = warcParser;
+		}
+
+
+		public WarcRecord? GetNext()
+		{
+
+			WarcRecord? ret = null;
+
+			lock (locker)
+			{
+				ret = Parser.GetNext();
+				if (ret != null)
+				{
+					Processed++;
+                    if (Processed % 100 == 0) Console.WriteLine($"Ingesting\t{Processed}");
+                }
+			}
+			return ret;
+		}
+	}
+}
+
