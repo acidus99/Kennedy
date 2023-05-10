ba36901644fc051dda87ea856f93ebdeb8d67186
Acidus <acidus99@users.noreply.github.com>
2023-05-10 16:33:12 -0400

    adds Stats page to Delorean

--- Server/Program.cs
+++ Server/Program.cs
@@ -35,9 +35,8 @@ namespace Kennedy.Server
             server.OnRequest(RoutePaths.ViewUrlHistoryRoute, ArchiveController.UrlHistory);
             server.OnRequest(RoutePaths.ViewCachedRoute, ArchiveController.Cached);
             server.OnRequest(RoutePaths.SearchArchiveRoute, ArchiveController.Search);
+            server.OnRequest(RoutePaths.ArchiveStatsRoute, ArchiveController.Stats);
             server.OnRequest("/delorean", ArchiveController.Redirect);
-
-
 
             server.OnRequest(RoutePaths.SiteHealthRoute, ReportsController.SiteHealth);
 
