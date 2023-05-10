f4a22e9bb20d6f0c8ce12f160dc2a9898cd8ad18
Acidus <acidus99@users.noreply.github.com>
2023-05-10 13:02:18 -0400

    Archive uses latest version of Robots.txt to clean archive

--- Indexer/WarcProcessors/ArchiveProcessor.cs
+++ Indexer/WarcProcessors/ArchiveProcessor.cs
@@ -1,6 +1,7 @@
 ﻿namespace Kennedy.Indexer.WarcProcessors;
 
 using System;
+using System.Linq;
 using Warc;
 
 using Gemini.Net;
@@ -9,23 +10,91 @@ using Kennedy.Archive;
 using Kennedy.Data;
 using Kennedy.Data.Parsers;
 using Kennedy.SearchIndex.Web;
+using Kennedy.Data.RobotsTxt;
+using Kennedy.Archive.Db;
 
 
 public class ArchiveProcessor : IWarcProcessor
 {
     Archiver archiver;
+    Dictionary<Authority, bool> changedAuthorities = new Dictionary<Authority, bool>();
 
-    public ArchiveProcessor(string archiveDirectory, string configDirectory)
+    public ArchiveProcessor(string archiveDirectory)
 	{
         archiver = new Archiver(archiveDirectory + "archive.db", archiveDirectory + "Packs/");
     }
 
     public void FinalizeProcessing()
 	{
+        foreach(var authority in changedAuthorities.Keys)
+        {
+            var robots = GetRobots(authority);
 
+            //no robots for domain that also contains archiver rules
+            if(robots == null)
+            {
+                continue;
+            }
+
+            using (var db = archiver.GetContext())
+            {
+                var urls = db.Urls.Where(x => x.Protocol == authority.Protocol &&
+                                    x.Domain == authority.Domain &&
+                                    x.Port == authority.Port);
+
+                foreach (var url in urls)
+                {
+                    bool allowed = robots.IsPathAllowed("archiver", url.GeminiUrl.Path);
+
+                    if (!allowed && url.IsPublic)
+                    {
+                        url.IsPublic = false;
+                        db.SaveChanges();
+                    }
+                    else if (allowed && !url.IsPublic)
+                    {
+                        url.IsPublic = true;
+                        db.SaveChanges();
+                    }
+                }
+            }
+        }
     }
 
-	public void ProcessRecord(WarcRecord record)
+    private RobotsTxtFile? GetRobots(Authority authority)
+    {
+        var robotsUrl = new GeminiUrl(RobotsTxtFile.CreateRobotsUrl(authority.Protocol, authority.Domain, authority.Port));
+
+        GeminiResponse? geminiResponse = archiver.GetLatestResponse(robotsUrl.ID);
+
+        if(geminiResponse == null)
+        {
+            return null;
+        }
+        if(!geminiResponse.IsSuccess || !geminiResponse.HasBody)
+        {
+            return null;
+        }
+
+        RobotsTxtFile robots = new RobotsTxtFile(geminiResponse.BodyText);
+        if(robots.IsMalformed)
+        {
+            return null;
+        }
+
+        //we only care about Robots.txt files that have archiver rules.
+        if (!robots.UserAgents.Contains("archiver"))
+        {
+            return null;
+        }
+
+        return robots;
+    }
+
+
+
+
+    public void ProcessRecord(WarcRecord record)
 	{
         if(record.Type == "response")
         {
@@ -38,14 +107,28 @@ public class ArchiveProcessor : IWarcProcessor
         var response = ParseResponseRecord(responseRecord);
         if (response != null)
         {
-            archiver.ArchiveResponse(response);
+            //only if we successfully archived it should we track that
+            //the domain should be checked against Robots.txt
+            if(archiver.ArchiveResponse(response))
+            {
+                var authority = new Authority
+                {
+                    Domain = response.RequestUrl.Hostname,
+                    Port = response.RequestUrl.Port,
+                    Protocol = response.RequestUrl.Protocol
+                };
+
+                if (!changedAuthorities.ContainsKey(authority))
+                {
+                    changedAuthorities.Add(authority, true);
+                }
+            }
         }
     }
 
     private GeminiResponse? ParseResponseRecord(ResponseRecord record)
     {
         GeminiUrl url = new GeminiUrl(StripRobots(record.TargetUri));
-
 
         try
         {
@@ -71,4 +154,10 @@ public class ArchiveProcessor : IWarcProcessor
         return url;
     }
 
+    private struct Authority
+    {
+        public string Domain { get; set; }
+        public int Port { get; set; }
+        public string Protocol { get; set; }
+    }
 }
