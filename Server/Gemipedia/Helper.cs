afc73af35f1c04f1c000ea5dc95295b76fc70058
Acidus <acidus99@users.noreply.github.com>
2023-05-17 12:25:47 -0400

    remove media proxying and searching options

--- Server/Gemipedia/Helper.cs
+++ Server/Gemipedia/Helper.cs
@@ -8,28 +8,7 @@ namespace Kennedy.Gemipedia
 	{
         const string GemipediaRoot = "gemini://gemi.dev/cgi-bin/wp.cgi/";
 
-        public static string ArticleUrl(string title)
-            => $"{GemipediaRoot}view?{WebUtility.UrlEncode(title)}";
-
-        public static string MediaProxyUrl(string imgUrl)
-        {
-            //we need to have an extension on the filename of the media proxy URL, so clients
-            //will render it as an inline image. Try and figure out what to use, but fall back
-            //to a dummy "jpg" if nothing works
-            string ext = ".jpg";
-            try
-            {
-                var uri = new Uri(imgUrl);
-                ext = Path.GetExtension(uri.AbsolutePath);
-            }
-            catch (Exception)
-            {
-                ext = ".jpg";
-            }
-            return $"{GemipediaRoot}media/media{ext}?{WebUtility.UrlEncode(imgUrl)}";
-        }
-
-        public static string SearchUrl(string query)
-            => $"{GemipediaRoot}search?{WebUtility.UrlEncode(query)}";
+        public static string ArticleUrl(ArticleSummary article)
+            => $"{GemipediaRoot}view?{WebUtility.UrlEncode(article.Title)}";
     }
 }
