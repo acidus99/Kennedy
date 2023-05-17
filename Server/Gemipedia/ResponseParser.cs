fbe1c98aaab3364d88ea64b9e7f4f28d686cd92a
Acidus <acidus99@users.noreply.github.com>
2023-05-17 12:26:34 -0400

    Simplify parsing for fewer properties

--- Server/Gemipedia/ResponseParser.cs
+++ Server/Gemipedia/ResponseParser.cs
@@ -9,58 +9,32 @@ using Newtonsoft.Json.Linq;
 
 namespace Kennedy.Gemipedia
 {
-	public static class ResponseParser
+	internal static class ResponseParser
 	{
         public static List<ArticleSummary> ParseSearchResponse(string json)
         {
-            var response = ParseJson(json);
             List<ArticleSummary> ret = new List<ArticleSummary>();
+            var response = JObject.Parse(json);
 
-            var resultsArray = response["pages"];
-            if(resultsArray == null || resultsArray is not JArray)
+            var resultsArray = response["pages"] as JArray;
+            if(resultsArray == null)
             {
                 return ret;
             }
 
-            foreach (JObject result in ((JArray) resultsArray))
+            foreach (JObject result in resultsArray)
             {
                 ret.Add(new ArticleSummary
                 {
                     Title = Cleanse(result["title"] as JToken),
-                    Excerpt = StripHtml(Cleanse(result["excerpt"])),
                     Description = Cleanse(result["description"]),
-                    ThumbnailUrl = GetThumbnailUrl(result["thumbnail"] as JObject)
                 });
             }
             return ret;
         }
 
-        private static string GetThumbnailUrl(JObject? thumb)
-        {
-            //result["thumbnail"]?["url"]? doesn't seem to work
-            if (thumb != null)
-            {
-                var url = thumb["url"]?.ToString() ??
-                            thumb["source"]?.ToString() ?? "";
-                if (url.Length > 0)
-                {
-                    return EnsureHttps(url);
-                }
-            }
-            return "";
-        }
-
-        private static string EnsureHttps(string url)
-            => url.StartsWith("https:") ? url : "https:" + url;
-
         private static string Cleanse(JToken? token)
             => token?.ToString() ?? "";
-
-        private static JObject ParseJson(string json)
-            => JObject.Parse(json);
-
-        private static string StripHtml(string s)
-            => WebUtility.HtmlDecode(Regex.Replace(s, @"<[^>]*>", "")) + "...";
     }
 }
 
