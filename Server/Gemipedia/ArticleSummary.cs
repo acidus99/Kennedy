6ab661ae6d14109792fad0e282eef20a1fab1ea3
Acidus <acidus99@users.noreply.github.com>
2023-05-17 12:25:17 -0400

    Simplify object and creation
    no need for most of those properties

--- Server/Gemipedia/ArticleSummary.cs
+++ Server/Gemipedia/ArticleSummary.cs
@@ -3,19 +3,7 @@ namespace Kennedy.Gemipedia
 {
     public class ArticleSummary
     {
-        public required string Title { get; set; }
-        public required string Description { get; set; }
-        public required string ThumbnailUrl { get; set; }
-
-        /// <summary>
-        /// Snippet of text where search term was found. Usually less helpful than description
-        /// </summary>
-        public required string Excerpt { get; set; }
-
-        public bool HasSummary
-            =>!string.IsNullOrEmpty(SummaryText);
-
-        public string SummaryText
-            => !String.IsNullOrEmpty(Description) ? Description : Excerpt;
+        public required string Title { get; init; }
+        public required string Description { get; init; }
     }
 }
