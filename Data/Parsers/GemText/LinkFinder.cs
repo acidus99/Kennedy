ec734c94fa3f5b2102b35dca02c05424f2bf06e2
Acidus <acidus99@users.noreply.github.com>
2023-07-22 16:01:50 -0400

    revert to regex, but add sanity check
    the regex isn't the perf issue, its the call to "new Uri()" so skip it for non-Gemini full qualified URLs

--- Data/Parsers/GemText/LinkFinder.cs
+++ Data/Parsers/GemText/LinkFinder.cs
@@ -14,7 +14,6 @@ namespace Kennedy.Data.Parsers.GemText
     public static class LinkFinder
     {
         static readonly Regex linkLine = new Regex(@"^=>\s*([^\s]+)\s*(.*)", RegexOptions.Compiled);
-        static readonly char[] splitChars = {' ', '\t' };
 
         /// <summary>
         /// Returns the link text from a link line. If not a link line, or no link text is present, returns ""
@@ -22,67 +21,44 @@ namespace Kennedy.Data.Parsers.GemText
         /// <param name="line"></param>
         /// <returns></returns>
         public static string GetLinkText(string line)
-        {
-            if (!line.StartsWith("=> "))
-            {
-                return "";
-            }
-
-            var parts = line.Split(splitChars, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-            //part[0] is "=>"
-            //part[1] is the url
-            //part[2..n] are the optional link text if any 
-
-            if (parts.Length > 2)
-            {
-                return string.Join(' ', parts.Skip(2));
-            }
-            return "";
-        }
+            => getLinkText(linkLine.Match(line));
 
         public static IEnumerable<FoundLink> GetLinks(GeminiUrl requestUrl, IEnumerable<string> bodyLines)
         {
-            List<FoundLink> ret = new List<FoundLink>();
-            foreach(string line in bodyLines)
-            {
-                string buffer = line;
-
-                if(!buffer.StartsWith("=>") || buffer.Length < 3)
-                {
-                    continue;
-                }
-                buffer = buffer.Substring(2).TrimStart();
-
-                var parts = buffer.Split(splitChars, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-                //part[0] is the url
-                //part[1..n] are the optional link text if any 
-
-                var url = parts[0];
-                //sanity check, if its a fully qualified URL, ensure its a gemini URL. otherwise, we really don't care
-                if (url.Contains("://") && !url.StartsWith("gemini://"))
-                {
-                    continue;
-                }
-
-                var newUrl = GeminiUrl.MakeUrl(requestUrl, url);
-                //ignore anything that doesn't resolve properly, or isn't to a gemini:// URL
-                if (newUrl != null)
-                {
-                    string linkText = "";
-                    //We have a link we care about, so reassemble the link text if any
-                    if(parts.Length > 1)
-                    {
-                        linkText = string.Join(' ', parts.Skip(1));
-                    }
-                    ret.Add(new FoundLink
-                    {
-                        Url = newUrl,
-                        IsExternal = (newUrl.Authority != requestUrl.Authority),
-                        LinkText = linkText
-                    });
-                }
-            }
-            return ret;
+            return (from line in bodyLines
+                    let match = linkLine.Match(line)
+                    where ShouldProcess(match)
+                    let link = Create(requestUrl, match)
+                    where link != null
+                    select link);
         }
+
+        private static bool ShouldProcess(Match match)
+        {
+            if(!match.Success)
+            {
+                return false;
+            }
+
+            var linkUrl = match.Groups[1].Value;
+
+            if (linkUrl.Contains("://") && !linkUrl.StartsWith("gemini://"))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static FoundLink? Create(GeminiUrl pageUrl, Match match)
+            => FoundLink.Create(pageUrl, match.Groups[1].Value, getLinkText(match));
+
+        /// <summary>
+        /// gives us the text, if any, used with this link
+        /// </summary>
+        /// <param name="match"></param>
+        /// <returns></returns>
+        private static string getLinkText(Match match)
+            => (match.Groups.Count > 2) ? match.Groups[2].Value : "";
+
     }
 }
