8c31d4ebe5dbb85079971ff24d740cd184c511d0
Acidus <acidus99@users.noreply.github.com>
2023-10-05 10:21:37 -0400

    comments

--- WarcConverters/MozzPortalImport/ArchivedContent.cs
+++ WarcConverters/MozzPortalImport/ArchivedContent.cs
@@ -5,10 +5,24 @@ using Gemini.Net;
 
 public class ArchivedContent
 {
+	/// <summary>
+	/// The URL we downloaded and parsed from the Wayback machine
+	/// </summary>
 	public required WaybackUrl Url { get; init; }
 
+	/// <summary>
+	/// An extracted, parsed GeminiResponse, representing the content from the Wayback machine, if any
+	/// </summary>
 	public GeminiResponse? GeminiResponse { get; set; }
 
+	/// <summary>
+	/// The X509 Certificate that was stored for a capsule
+	/// </summary>
 	public X509Certificate2? Certificate { get; set; }
+
+	/// <summary>
+	/// Any additional URLs we should try
+	/// </summary>
+	public List<WaybackUrl> MoreUrls { get; } = new List<WaybackUrl>();
 }
 
