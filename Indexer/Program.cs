593908ea4382ee1e67a272a2b4b083bfd7f6faed
Acidus <acidus99@users.noreply.github.com>
2023-12-11 07:55:06 -0500

    namespace + using

--- Indexer/Program.cs
+++ Indexer/Program.cs
@@ -1,85 +1,71 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using Kennedy.Indexer.WarcProcessors;
 
-using Gemini.Net;
-using Kennedy.Data.Parsers;
+namespace Kennedy.Indexer;
 
-using Kennedy.SearchIndex.Search;
-
-using Kennedy.Warc;
-
-using WarcDotNet;
-
-using Kennedy.Indexer.WarcProcessors;
-using System.Diagnostics;
-
-namespace Kennedy.Indexer
+class Program
 {
-    class Program
+    /// <summary>
+    /// Consumes a WARC and generates a search index
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
     {
-        /// <summary>
-        /// Consumes a WARC and generates a search index
-        /// </summary>
-        /// <param name="args"></param>
-        static void Main(string[] args)
+
+        Console.WriteLine("Kennedy Indexer");
+        if (args.Length != 2)
         {
-
-            Console.WriteLine("Kennedy Indexer");
-            if (args.Length != 2)
-            {
-                Console.WriteLine("Incorrect number of parameters.");
-                Console.WriteLine("Usage: Indexer [Output Directory] [Input WARC file]");
-                return;
-            }
-
-            string outputDir = args[0];
-            string inputWarc = args[1];
-
-            if(!Directory.Exists(outputDir))
-            {
-                Console.WriteLine($"Cannot read output directory '{outputDir}'");
-                return;
-            }
-
-            if(!File.Exists(inputWarc))
-            {
-                Console.WriteLine($"Cannot read input WARC file '{inputWarc}'");
-                return;
-            }
-
-            string configDir = ResolveDir("~/kennedy-capsule/config/");
-
-            IWarcProcessor searchProcessor = new SearchProcessor(outputDir, configDir);
-            IWarcProcessor archiveProcessor = new ArchiveProcessor(outputDir, configDir);
-
-            ProcessWarc(inputWarc, searchProcessor, archiveProcessor);
+            Console.WriteLine("Incorrect number of parameters.");
+            Console.WriteLine("Usage: Indexer [Output Directory] [Input WARC file]");
+            return;
         }
 
-        static void ProcessWarc(string inputWarc, params IWarcProcessor [] processors)
+        string outputDir = args[0];
+        string inputWarc = args[1];
+
+        if(!Directory.Exists(outputDir))
         {
-            using (WarcReader reader = new WarcReader(inputWarc))
+            Console.WriteLine($"Cannot read output directory '{outputDir}'");
+            return;
+        }
+
+        if(!File.Exists(inputWarc))
+        {
+            Console.WriteLine($"Cannot read input WARC file '{inputWarc}'");
+            return;
+        }
+
+        string configDir = ResolveDir("~/kennedy-capsule/config/");
+
+        IWarcProcessor searchProcessor = new SearchProcessor(outputDir, configDir);
+        IWarcProcessor archiveProcessor = new ArchiveProcessor(outputDir, configDir);
+
+        ProcessWarc(inputWarc, searchProcessor, archiveProcessor);
+    }
+
+    static void ProcessWarc(string inputWarc, params IWarcProcessor [] processors)
+    {
+        using (WarcReader reader = new WarcReader(inputWarc))
+        {
+            DateTime prev = DateTime.Now;
+            foreach (WarcRecord record in reader)
             {
-                DateTime prev = DateTime.Now;
-                foreach (WarcRecord record in reader)
-                {
-                    foreach (var processor in processors)
-                    {
-                        processor.ProcessRecord(record);
-                    }
-                    if (reader.RecordsRead % 100 == 0)
-                    {
-                        Console.WriteLine($"{reader.Filename}\t{reader.RecordsRead}\t{Math.Truncate(DateTime.Now.Subtract(prev).TotalMilliseconds)} ms");
-                        prev = DateTime.Now;
-                    }
-                }
                 foreach (var processor in processors)
                 {
-                    processor.FinalizeProcessing();
+                    processor.ProcessRecord(record);
+                }
+                if (reader.RecordsRead % 100 == 0)
+                {
+                    Console.WriteLine($"{reader.Filename}\t{reader.RecordsRead}\t{Math.Truncate(DateTime.Now.Subtract(prev).TotalMilliseconds)} ms");
+                    prev = DateTime.Now;
                 }
             }
+            foreach (var processor in processors)
+            {
+                processor.FinalizeProcessing();
+            }
         }
-
-        private static string ResolveDir(string dir)
-            => dir.Replace("~/", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + '/');
     }
+
+    private static string ResolveDir(string dir)
+        => dir.Replace("~/", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + '/');
 }
