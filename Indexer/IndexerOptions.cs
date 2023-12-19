55b63af3d91d94d8cd27803cb88562c0005454df
Acidus <acidus99@users.noreply.github.com>
2023-12-19 10:20:01 -0500

    refactor to use better command line options

---
+++ Indexer/IndexerOptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Kennedy.Indexer
+{
+	public class IndexerOptions
+	{
+        /// <summary>
+        /// WARC file to index
+        /// </summary>
+        public List<string> InputWarcs { get; } = new List<string>();
+
+        /// <summary>
+        /// Location of the index
+        /// </summary>
+        public string OutputLocation { get; set; } = "";
+
+        /// <summary>
+        /// Should we index the input for the archive?
+        /// </summary>
+        public bool ShouldIndexArchive { get; set; } = false;
+
+        /// <summary>
+        /// Should we index the input for search?
+        /// </summary>
+        public bool ShouldIndexCrawl { get; set; } = false;
+
+        public bool ShowHelp { get; set; } = false;
+    }
+}
+
--- Indexer/Program.cs
+++ Indexer/Program.cs
@@ -1,52 +1,107 @@
-﻿using Kennedy.Indexer.WarcProcessors;
+﻿using System.Diagnostics;
+using Kennedy.Indexer.WarcProcessors;
+using Mono.Options;
+using WarcDotNet;
 
 namespace Kennedy.Indexer;
-
 class Program
 {
+
+    static string ProgramName
+        => Process.GetCurrentProcess().MainModule?.FileName ?? "Program";
+
     /// <summary>
     /// Consumes a WARC and generates a search index
     /// </summary>
     /// <param name="args"></param>
     static void Main(string[] args)
     {
+        IndexerOptions options = ParseOptions(args);
+        ValidateOptions(options);
+        DisplaySummary(options);
+        IEnumerable<IWarcProcessor> processors = CreateProcessors(options);
 
-        Console.WriteLine("Kennedy Indexer");
-        if (args.Length != 2)
+        foreach(var inputWarc in options.InputWarcs)
         {
-            Console.WriteLine("Incorrect number of parameters.");
-            Console.WriteLine("Usage: Indexer [Output Directory] [Input WARC file]");
-            return;
+            ProcessWarc(inputWarc, processors);
         }
 
-        string outputDir = args[0];
-        string inputWarc = args[1];
-
-        if(!Directory.Exists(outputDir))
-        {
-            Console.WriteLine($"Cannot read output directory '{outputDir}'");
-            return;
-        }
-
-        if(!File.Exists(inputWarc))
-        {
-            Console.WriteLine($"Cannot read input WARC file '{inputWarc}'");
-            return;
-        }
-
-        string configDir = ResolveDir("~/kennedy-capsule/config/");
-
-        IWarcProcessor searchProcessor = new SearchProcessor(outputDir, configDir);
-        IWarcProcessor archiveProcessor = new ArchiveProcessor(outputDir, configDir);
-
-        ProcessWarc(inputWarc, searchProcessor, archiveProcessor);
     }
 
-    static void ProcessWarc(string inputWarc, params IWarcProcessor [] processors)
+    /// <summary>
+    /// Creates needed processors for the given options
+    /// </summary>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    static IEnumerable<IWarcProcessor> CreateProcessors(IndexerOptions options)
+    {
+        string configDir = ResolveDir("~/kennedy-capsule/config/");
+
+        var ret = new List<IWarcProcessor>();
+        if(options.ShouldIndexArchive)
+        {
+            ret.Add(new ArchiveProcessor(options.OutputLocation, configDir));
+        }
+        if(options.ShouldIndexCrawl)
+        {
+            ret.Add(new SearchProcessor(options.OutputLocation, configDir));
+        }
+        return ret;
+    }
+
+    static void DisplayError(string errorMsg)
+    {
+        Console.Error.WriteLine(errorMsg);
+        Environment.Exit(1);
+    }
+
+    static void DisplaySummary(IndexerOptions options)
+    {
+        Console.WriteLine(ProgramName);
+        Console.WriteLine($"Indexing {options.InputWarcs.Count} file(s)");
+        Console.WriteLine($"Index location\t'{options.OutputLocation}'");
+        Console.Write("Processors:\t");
+        if(options.ShouldIndexArchive)
+        {
+            Console.Write("archiver ");
+        }
+        if(options.ShouldIndexCrawl)
+        {
+            Console.Write("search ");
+        }
+        Console.WriteLine();
+    }
+
+    static IndexerOptions ParseOptions(string[] args)
+    {
+        var ret = new IndexerOptions();
+        bool showHelp = false;
+
+        var options = new OptionSet {
+            { "a|archive", "index input into the archive", a => ret.ShouldIndexArchive = true },
+            { "o|output=", "output location", o => ret.OutputLocation = o ?? "" },
+            { "s|search", "index input into search", s => ret.ShouldIndexCrawl = true },
+            { "h|help", "show this message and exit", h => showHelp = h != null },
+        };
+
+        ret.InputWarcs.AddRange(options.Parse(args));
+
+        if (showHelp)
+        {
+            Console.WriteLine(ProgramName);
+            options.WriteOptionDescriptions(Console.Out);
+            Environment.Exit(0);
+        }
+
+        return ret;
+    }
+
+    static void ProcessWarc(string inputWarc, IEnumerable<IWarcProcessor> processors)
     {
         using (WarcReader reader = new WarcReader(inputWarc))
         {
-            DateTime prev = DateTime.Now;
+            DateTime start = DateTime.Now;
+            DateTime prev = start;
             foreach (WarcRecord record in reader)
             {
                 foreach (var processor in processors)
@@ -55,10 +110,15 @@ class Program
                 }
                 if (reader.RecordsRead % 100 == 0)
                 {
-                    Console.WriteLine($"{reader.Filename}\t{reader.RecordsRead}\t{Math.Truncate(DateTime.Now.Subtract(prev).TotalMilliseconds)} ms");
+                    var elapsedSeconds = Math.Truncate(DateTime.Now.Subtract(start).TotalSeconds);
+                    var ratePerSecond = Math.Truncate(reader.RecordsRead / elapsedSeconds);
+                    Console.Write($"{reader.Filename}\t{reader.RecordsRead}\t {elapsedSeconds} s ({ratePerSecond} / s)    ");
+                    Console.Write('\r');
                     prev = DateTime.Now;
                 }
             }
+            Console.WriteLine();
+            Console.WriteLine("Post processing");
             foreach (var processor in processors)
             {
                 processor.FinalizeProcessing();
@@ -68,4 +128,36 @@ class Program
 
     private static string ResolveDir(string dir)
         => dir.Replace("~/", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + '/');
+
+    static void ValidateOptions(IndexerOptions options)
+    {
+        if(!options.ShouldIndexArchive && !options.ShouldIndexCrawl)
+        {
+            DisplayError("Must include an option to index for search (-s or --search), for archive (-a or --archive), or for both.");
+        }
+
+        if(string.IsNullOrEmpty(options.OutputLocation))
+        {
+            DisplayError("Must include an option for the output location (-o DIRECTORY or --output DIRECTORY).");
+        }
+
+        if (!Directory.Exists(options.OutputLocation))
+        {
+            DisplayError($"Cannot read output directory '{options.OutputLocation}'.");
+        }
+
+        if (options.InputWarcs.Count < 1)
+        {
+            DisplayError("Must include at least 1 WARC file to process. e.g. Indexer -s -o /some/directory/ some-file.warc");
+        }
+
+        foreach(var inputWarc in options.InputWarcs)
+        {
+            if (!File.Exists(inputWarc))
+            {
+                DisplayError($"Cannot read input WARC file '{inputWarc}'");
+            }
+        }
+    }
+
 }
