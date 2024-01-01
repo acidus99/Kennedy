c7d3e2b40344533670ef425761c6003e3687236b
Acidus <acidus99@users.noreply.github.com>
2024-01-01 17:25:10 -0500

    Don't ensure directory when loading a PackFile, do it only if appending to a PackFile

--- Archive/Pack/PackFile.cs
+++ Archive/Pack/PackFile.cs
@@ -1,59 +1,60 @@
-﻿using System;
-using System.IO;
-namespace Kennedy.Archive.Pack
+﻿namespace Kennedy.Archive.Pack;
+
+public class PackFile
 {
-	public class PackFile
-	{
-		public string Filename { get; private set; }
+    string FullPath;
+    string Path;
 
-		public PackFile(string filename)
-		{
-			Filename = filename;
-		}
+    public PackFile(string path, string filename)
+    {
+        Path = path;
+        FullPath = Path + filename;
+    }
 
-		public long Append(PackRecord packRecord)
-		{
-			var offset = GetOffset();
-			using (var fout = new FileStream(Filename, FileMode.Append))
-			{
-				var data = PackRecordFactory.ToBytes(packRecord);
-				fout.Write(data);
-			}
-			return offset;
-		}
-
-		public PackRecord Read(long offset)
-		{
-			using (var fin = new BinaryReader(new FileStream(Filename, FileMode.Open)))
-			{
-				fin.BaseStream.Seek(offset, SeekOrigin.Begin);
-				string type = GetType(fin.ReadBytes(4));
-				long len = Convert.ToInt64(fin.ReadUInt32());
-
-				var data = fin.ReadBytes((int)len);
-				return new PackRecord
-				{
-					Type = type,
-					Data = data
-				};
-			}
-		}
-
-		private long GetOffset()
+    public long Append(PackRecord packRecord)
+    {
+        var offset = GetOffset();
+        //ensure the path exists
+        Directory.CreateDirectory(Path);
+        using (var fout = new FileStream(FullPath, FileMode.Append))
         {
-			try
-            {
-				return (new FileInfo(Filename)).Length;
-			} catch(Exception)
-            {
+            var data = PackRecordFactory.ToBytes(packRecord);
+            fout.Write(data);
+        }
+        return offset;
+    }
 
-            }
-			//file doesn't exist, so the offset is zero
-			return 0;
-        } 
+    public PackRecord Read(long offset)
+    {
+        using (var fin = new BinaryReader(new FileStream(FullPath, FileMode.Open)))
+        {
+            fin.BaseStream.Seek(offset, SeekOrigin.Begin);
+            string type = GetType(fin.ReadBytes(4));
+            long len = Convert.ToInt64(fin.ReadUInt32());
 
-		private string GetType(byte[] type)
-			=> System.Text.Encoding.ASCII.GetString(type);
-	}
+            var data = fin.ReadBytes((int)len);
+            return new PackRecord
+            {
+                Type = type,
+                Data = data
+            };
+        }
+    }
+
+    private long GetOffset()
+    {
+        try
+        {
+            return (new FileInfo(FullPath)).Length;
+        }
+        catch (Exception)
+        {
+        }
+        //file doesn't exist, so the offset is zero
+        return 0;
+    }
+
+    private string GetType(byte[] type)
+        => System.Text.Encoding.ASCII.GetString(type);
 }
 
--- Archive/Pack/PackManager.cs
+++ Archive/Pack/PackManager.cs
@@ -1,112 +1,106 @@
-﻿namespace Kennedy.Archive.Pack
-{
-    /// <summary>
-    /// Stores a group of packs on disk and an efficient way
-    /// </summary>
-    public class PackManager
-    {
-        string ArchiveRoot;
-        char[] InvalidPathChars;
-        char[] InvalidFileChars;
+﻿namespace Kennedy.Archive.Pack;
 
-        public PackManager(string path)
+/// <summary>
+/// Stores a group of packs on disk and an efficient way
+/// </summary>
+public class PackManager
+{
+    string ArchiveRoot;
+    char[] InvalidPathChars;
+    char[] InvalidFileChars;
+
+    public PackManager(string path)
+    {
+        ArchiveRoot = path;
+        if (!ArchiveRoot.EndsWith(Path.DirectorySeparatorChar))
         {
-            ArchiveRoot = path;
-            if (!ArchiveRoot.EndsWith(Path.DirectorySeparatorChar))
+            ArchiveRoot += Path.DirectorySeparatorChar;
+        }
+        InvalidPathChars = Path.GetInvalidPathChars();
+        InvalidFileChars = Path.GetInvalidFileNameChars();
+    }
+
+    /// <summary>
+    /// checks that a key doesn't contain illegal characters (e.g. illegal characters for path or filename)
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    private bool IsKeyIsValid(string key)
+    {
+        for (int i = 0, len = key.Length; i < len; i++)
+        {
+            char c = key[i];
+            if (i < 4 && InvalidPathChars.Contains(c))
             {
-                ArchiveRoot += Path.DirectorySeparatorChar;
+                return false;
             }
-            InvalidPathChars = Path.GetInvalidPathChars();
-            InvalidFileChars = Path.GetInvalidFileNameChars();
+            if (InvalidFileChars.Contains(c))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private string GetPathForPackName(string packName)
+    {
+        return ArchiveRoot + Path.DirectorySeparatorChar + packName[0] + packName[1] + Path.DirectorySeparatorChar +
+            packName[2] + packName[3] + Path.DirectorySeparatorChar;
+    }
+
+    public PackFile GetPack(string dataHash)
+    {
+        var index = dataHash.IndexOf(':');
+
+        if (index < 0 || index == dataHash.Length)
+        {
+            throw new ArgumentException("Hash is not of the form '[hash name]:[hexadecimal string]'");
         }
 
-        /// <summary>
-        /// checks that a key doesn't contain illegal characters (e.g. illegal characters for path or filename)
-        /// </summary>
-        /// <param name="key"></param>
-        /// <returns></returns>
-        private bool IsKeyIsValid(string key)
+        dataHash = dataHash.Substring(index + 1);
+
+        if (dataHash.Length < 4)
         {
-            for (int i = 0, len = key.Length; i < len; i++)
-            {
-                char c = key[i];
-                if (i < 4 && InvalidPathChars.Contains(c))
-                {
-                    return false;
-                }
-                if (InvalidFileChars.Contains(c))
-                {
-                    return false;
-                }
-            }
+            throw new ArgumentException($"PackID is too short! Expected > 4, got {dataHash.Length}");
+        }
+
+        if (!IsKeyIsValid(dataHash))
+        {
+            throw new ArgumentException("Packname contains invalid characters", "packName");
+        }
+
+        var path = GetPathForPackName(dataHash);
+        return new PackFile(path, GetPackFileName(dataHash));
+    }
+
+    private string GetPackFileName(string packName)
+    {
+        if (packName.Length < 4)
+        {
+            throw new ArgumentException($"PackID is too short! Expected > 4, got {packName.Length}");
+        }
+
+        if (!IsKeyIsValid(packName))
+        {
+            throw new ArgumentException("Packname contains invalid characters", "packName");
+        }
+        return $"{packName[0]}{packName[1]}{packName[2]}{packName[3]}";
+    }
+
+
+    public bool DeletePack(string packName)
+    {
+        if (!IsKeyIsValid(packName))
+        {
+            throw new ArgumentException("Packname contains invalid characters", "packName");
+        }
+
+        var path = GetPathForPackName(packName);
+        if (File.Exists(path + packName))
+        {
+            File.Delete(path + packName);
             return true;
         }
-
-        private string getPathForPackName(string packName)
-        {
-            return ArchiveRoot + Path.DirectorySeparatorChar + packName[0] + packName[1] + Path.DirectorySeparatorChar +
-                packName[2] + packName[3] + Path.DirectorySeparatorChar;
-        }
-
-        public PackFile GetPack(string dataHash)
-        {
-
-            var index = dataHash.IndexOf(':');
-
-            if (index < 0 || index == dataHash.Length)
-            {
-                throw new ArgumentException("Hash is not of the form '[hash name]:[hexadecimal string]'");
-            }
-
-            dataHash = dataHash.Substring(index + 1);
-
-            if (dataHash.Length < 4)
-            {
-                throw new ArgumentException($"PackID is too short! Expected > 4, got {dataHash.Length}");
-            }
-
-            if (!IsKeyIsValid(dataHash))
-            {
-                throw new ArgumentException("Packname contains invalid characters", "packName");
-            }
-
-            var path = getPathForPackName(dataHash);
-
-            //Ensure the file path exists
-            Directory.CreateDirectory(path);
-
-            return new PackFile(path + GetPackFileName(dataHash));
-        }
-
-        private string GetPackFileName(string packName)
-        {
-            if (packName.Length < 4)
-            {
-                throw new ArgumentException($"PackID is too short! Expected > 4, got {packName.Length}");
-            }
-
-            if (!IsKeyIsValid(packName))
-            {
-                throw new ArgumentException("Packname contains invalid characters", "packName");
-            }
-            return $"{packName[0]}{packName[1]}{packName[2]}{packName[3]}";
-        }
-
-
-        public bool DeletePack(string packName)
-        {
-            if (!IsKeyIsValid(packName))
-            {
-                throw new ArgumentException("Packname contains invalid characters", "packName");
-            }
-
-            var path = getPathForPackName(packName);
-            if(File.Exists(path + packName))
-            {
-                File.Delete(path + packName);
-                return true;
-            }
-            return false;
-        }
+        return false;
     }
 }
