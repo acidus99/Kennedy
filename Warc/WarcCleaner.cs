506a45814b4c59dc9815e05e1109a08b52d5b1f6
Acidus <acidus99@users.noreply.github.com>
2024-01-09 07:52:25 -0500

    fix old TLS records

--- Warc/WarcCleaner.cs
+++ Warc/WarcCleaner.cs
@@ -10,8 +10,174 @@ namespace Kennedy.Warc
     /// </summary>
 	public static class WarcCleaner
     {
+        /// <summary>
+        /// Fix TLS info that is on 49 responses (since the vast majority they didn't get TLS completed, and are using the value
+        /// in the requestor from the previous response. Also, metadata records need to be removed too
+        /// </summary>
+        /// <param name="inputWarc"></param>
+        /// <param name="outputWarc"></param>
+        public static void FixTls(string inputWarc, string outputWarc)
+        {
+            DateTime prev = DateTime.Now;
+            int Processed = 0;
+
+            Dictionary<string, bool> Authorities = new Dictionary<string, bool>();
+
+            //first read through and find the authority for all capsules that had a "49" status 
+            using (WarcReader reader = new WarcReader(inputWarc))
+            {
+                foreach (var record in reader)
+                {
+                    if (record is ResponseRecord responseRecord && record.ContentBlock != null && responseRecord.TargetUri != null)
+                    {
+                        GeminiUrl url = new GeminiUrl(responseRecord.TargetUri);
+
+                        GeminiResponse geminiResponse = GeminiParser.ParseResponseBytes(url, record.ContentBlock);
+                        if (geminiResponse.StatusCode == 49)
+                        {
+                            Authorities[url.Authority] = true;
+                        }
+                    }
+                }
+            }
+
+            int cleaned = 0;
+            int noworkneeded = 0;
+            int metadata = 0;
+
+            using (WarcWriter writer = new WarcWriter(outputWarc))
+            {
+                using (WarcReader reader = new WarcReader(inputWarc))
+                {
+                    foreach (var record in reader)
+                    {
+                        bool shouldWrite = true;
+                        Processed++;
+
+                        if (record is WarcInfoRecord infoRecord)
+                        {
+                            //Clear any block digest from a warcinfo
+                            infoRecord.BlockDigest = null;
+                        }
+
+                        if (record is RequestRecord requestRecord && requestRecord.TargetUri != null)
+                        {
+
+                            //Clear any payload digest from a request
+                            requestRecord.PayloadDigest = null;
+
+                            //Fix the TLS stuff
+                            FixWarcProtocol(record);
+                            FixTlsCipherSuite(record);
+
+                            //Check if this is something where we should remove the TLS info altogether
+                            GeminiUrl url = new GeminiUrl(requestRecord.TargetUri);
+                            if (Authorities.ContainsKey(url.Authority))
+                            {
+                                if (record.CustomFields.Count > 0)
+                                {
+                                    cleaned++;
+                                    RemoveAllTlsFields(record);
+                                }
+                                else
+                                {
+                                    noworkneeded++;
+                                }
+                            }
+                        }
+
+                        if (record is ResponseRecord responseRecord && responseRecord.TargetUri != null)
+                        {
+                            //Fix the TLS stuff
+                            FixWarcProtocol(record);
+                            FixTlsCipherSuite(record);
+
+                            //Check if this is something where we should remove the TLS info altogether
+                            GeminiUrl url = new GeminiUrl(responseRecord.TargetUri);
+                            if (Authorities.ContainsKey(url.Authority))
+                            {
+                                if (record.CustomFields.Count > 0)
+                                {
+                                    cleaned++;
+                                    RemoveAllTlsFields(record);
+                                }
+                                else
+                                {
+                                    noworkneeded++;
+                                }
+                            }
+                        }
+
+                        if (record is MetadataRecord metadataRecord && metadataRecord.TargetUri != null)
+                        {
+                            GeminiUrl url = new GeminiUrl(metadataRecord.TargetUri);
+                            if (Authorities.ContainsKey(url.Authority))
+                            {
+                                //don't write out incorrect certificate record
+                                shouldWrite = false;
+                                metadata++;
+                            }
+                        }
+                        if (shouldWrite)
+                        {
+                            writer.Write(record);
+                        }
+                    }
+                }
+            }
+
+            int seconds = (int)DateTime.Now.Subtract(prev).TotalSeconds;
+            if (seconds == 0)
+            {
+                seconds = 1;
+            }
+
+            Console.WriteLine($"Records: {Processed}\tTime: {seconds}s\tRate: {Processed / seconds} / s");
+        }
+
+        private static void RemoveAllTlsFields(WarcRecord record)
+        {
+            //remove incorrect TLS fields
+            record.CustomFields.RemoveAll(GeminiWarcCreator.WarcProtocolField);
+            record.CustomFields.RemoveAll(GeminiWarcCreator.WarcCipherSuiteField);
+            //even the prototype field name
+            record.CustomFields.RemoveAll("WARC-TLS-Cipher-Suite");
+        }
+
+        private static void FixWarcProtocol(WarcRecord record)
+        {
+            if (record.CustomFields.FieldCount(GeminiWarcCreator.WarcProtocolField) == 1)
+            {
+                string val = record.CustomFields[GeminiWarcCreator.WarcProtocolField].First();
+                record.CustomFields.RemoveAll(GeminiWarcCreator.WarcProtocolField);
+                record.CustomFields.Add(GeminiWarcCreator.WarcProtocolField, "gemini");
+                record.CustomFields.Add(GeminiWarcCreator.WarcProtocolField, val);
+            }
+        }
+
+        private static void FixTlsCipherSuite(WarcRecord record)
+        {
+            const string tlsCipherField = "WARC-TLS-Cipher-Suite";
+
+            //using the old version?
+            if (record.CustomFields.ContainsField(tlsCipherField))
+            {
+                //update it
+                string val = record.CustomFields[tlsCipherField].First();
+                record.CustomFields.RemoveAll(tlsCipherField);
+                record.CustomFields.Add(GeminiWarcCreator.WarcCipherSuiteField, val);
+            }
+            else if (record.CustomFields.ContainsField(GeminiWarcCreator.WarcCipherSuiteField))
+            {
+                //fix the weird lower-casing that happened by removing and re-adding
+                string val = record.CustomFields[GeminiWarcCreator.WarcCipherSuiteField].First();
+                record.CustomFields.RemoveAll(GeminiWarcCreator.WarcCipherSuiteField);
+                record.CustomFields.Add(GeminiWarcCreator.WarcCipherSuiteField, val);
+            }
+        }
+
         public static void Fix(string inputWarc, string outputWarc)
-		{
+        {
             DateTime prev = DateTime.Now;
             int Processed = 0;
             using (WarcWriter writer = new WarcWriter(outputWarc))
@@ -44,8 +210,8 @@ namespace Kennedy.Warc
                 }
             }
 
-            int seconds = (int) DateTime.Now.Subtract(prev).TotalSeconds;
-            if(seconds == 0)
+            int seconds = (int)DateTime.Now.Subtract(prev).TotalSeconds;
+            if (seconds == 0)
             {
                 seconds = 1;
             }
@@ -60,10 +226,10 @@ namespace Kennedy.Warc
                 record.BlockDigest = GeminiParser.GetStrongHash(record.ContentBlock!);
             }
         }
-        
+
         private static void FixRobotsRequestRecord(RequestRecord requestRecord)
         {
-            if(requestRecord.TargetUri == null)
+            if (requestRecord.TargetUri == null)
             {
                 return;
             }
@@ -103,18 +269,18 @@ namespace Kennedy.Warc
         private static void FixResponsePayload(ResponseRecord responseRecord)
         {
 
-            if(responseRecord.IdentifiedPayloadType != null && responseRecord.PayloadDigest != null)
+            if (responseRecord.IdentifiedPayloadType != null && responseRecord.PayloadDigest != null)
             {
                 //nothing to do here
                 return;
             }
 
-            if(responseRecord.TargetUri == null)
+            if (responseRecord.TargetUri == null)
             {
                 throw new ArgumentException("Target URI is null!");
             }
 
-            if(responseRecord.ContentBlock == null)
+            if (responseRecord.ContentBlock == null)
             {
                 throw new ArgumentException("Content Block is empty?!?!");
             }
@@ -128,7 +294,7 @@ namespace Kennedy.Warc
                 responseRecord.IdentifiedPayloadType = response.MimeType;
             }
 
-            if(responseRecord.PayloadDigest == null && response.HasBody)
+            if (responseRecord.PayloadDigest == null && response.HasBody)
             {
                 responseRecord.PayloadDigest = GeminiParser.GetStrongHash(response.BodyBytes!);
             }
@@ -155,6 +321,6 @@ namespace Kennedy.Warc
         //}
 
 
-	}
+    }
 }
 
