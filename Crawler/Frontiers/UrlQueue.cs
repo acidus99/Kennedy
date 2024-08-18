1117cf2c360b7d0cc668ed585fdec1bb5d1deb3a
Acidus <acidus99@users.noreply.github.com>
2024-08-18 16:18:10 -0400

    Further simplify priority used for probable non-text URLS

--- Crawler/Frontiers/UrlQueue.cs
+++ Crawler/Frontiers/UrlQueue.cs
@@ -41,13 +41,8 @@ internal class UrlQueue
             return priority;
         }
 
-        //for non-text responses, lower the priority
-        priority *=5;
-
-        if(priority > 50000)
-        {
-            priority = 5000;
-        }
+        //for non-text responses, delay it
+        priority +=5000;
 
         return priority;
     }
@@ -57,21 +52,6 @@ internal class UrlQueue
 
     private bool IsTextRequest(GeminiUrl url)
         => (url.FileExtension == "txt");
-
-
-    private int CountDirectories(string path)
-    {
-        int ret = 0;
-        foreach (char c in path)
-        {
-            if (c == '/')
-            {
-                ret++;
-            }
-        }
-        //-1 because every URL starts with a /
-        return ret - 1;
-    }
 
     public UrlFrontierEntry? GetUrl()
     {
